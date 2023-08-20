d7d1dba87117e74d29d8e74df21309fede1c0513
claudiaacchurch <claudiachurch00@gmail.com>
2023-08-20 19:29:07 +0100

    both domains

--- api/app.js
+++ api/app.js
@@ -10,8 +10,20 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+const allowedOrigins = [
+  'https://frontend-ajg63g5uq-claudiaacchurch.vercel.app',
+  'https://frontend-phi-lyart.vercel.app'
+];
+
 app.use(cors({
-  origin: 'frontend-ajg63g5uq-claudiaacchurch.vercel.app'
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);  
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
 }));
 
 // view engine setup
