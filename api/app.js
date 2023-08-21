02b5787244c908c709bf59b95ebd351c96ca915f
claudiaacchurch <claudiachurch00@gmail.com>
2023-08-21 14:00:42 +0100

    reconfiged

--- api/app.js
+++ api/app.js
@@ -1,18 +1,17 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-const cors = require("cors");
-const bp = require("body-parser");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const express = require('express');
+const { createError } = require('http-errors');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const bp = require('body-parser');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 const dotenv = require('dotenv');
-require('dotenv').config();
 
 dotenv.config({ path: '.env.development' });
-var app = express();
 
+const app = express();
 const allowedOrigins = process.env.ALLOWED_ORIGINS;
 
 app.use(cors({
@@ -20,7 +19,6 @@ app.use(cors({
   origin: allowedOrigins
 }));
 
-// view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
@@ -32,30 +30,23 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
-
-
-
-//route setup
 app.use("/", indexRouter);
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is running!" });
 });
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
-module.exports = app;
+const { createServer } = require('http');
+const server = createServer(app);
+module.exports = server;
