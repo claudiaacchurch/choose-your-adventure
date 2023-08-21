1ee30545ab03d62fbe0abec7ff89d80d9741d1d7
claudiaacchurch <claudiachurch00@gmail.com>
2023-08-21 12:38:17 +0100

    added cors methods

--- api/app.js
+++ api/app.js
@@ -17,14 +17,18 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
 
 app.use(cors({
   origin: function (origin, callback) {
+    
     if (!origin) return callback(null, true);  
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
-  }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
+
+
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
